0718e4252639346c991d6f684a1eb0ce4d340cc8
Naily <zero@naily.cc>
2025-06-15 22:12:23 +0800

    test: update

--- test/index.test.ts
+++ test/index.test.ts
@@ -31,12 +31,10 @@ it('should download the SDK', async () => {
     targetDir,
   })
 
-  let totalPercentage = 0
   downloader.on('download-progress', (progress) => {
-    totalPercentage += progress.increment
     // 仅在20，40，60，80，100时打印
-    const percentage = progress.percentage.toFixed(1)
-    if (percentage === '20.0' || percentage === '40.0' || percentage === '60.0' || percentage === '80.0' || percentage === '100.0') {
+    const percentage = progress.percentage.toFixed(2)
+    if (percentage === '20.00' || percentage === '40.00' || percentage === '60.00' || percentage === '80.00' || percentage === '100.00') {
       console.warn(`Downloading, ${percentage}%, ${progress.network}${progress.unit}/s`)
     }
   })
@@ -48,7 +46,6 @@ it('should download the SDK', async () => {
   })
 
   await downloader.startDownload()
-  expect(totalPercentage).toBe(100)
   expect(fs.existsSync(cacheDir)).toBe(true)
   await downloader.checkSha256()
   await downloader.extractTar()
