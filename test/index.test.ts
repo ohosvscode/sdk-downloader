c268855316975f84ed1f53883a190bec3fd9e690
Naily <zero@naily.cc>
2025-06-15 21:49:33 +0800

    feat: add test

--- test/index.test.ts
+++ test/index.test.ts
@@ -1,3 +1,57 @@
-it('should pass', () => {
-  expect(1 + 1).toBe(2)
-})
+import fs from 'node:fs'
+import path from 'node:path'
+import { createDownloader, SdkArch, SdkOS, SdkVersion } from '../src'
+
+function getArch(): SdkArch {
+  if (process.arch === 'arm' || process.arch === 'arm64')
+    return SdkArch.ARM
+  else
+    return SdkArch.X86
+}
+
+function getOS(): SdkOS {
+  if (process.platform === 'darwin')
+    return SdkOS.MacOS
+  else if (process.platform === 'linux')
+    return SdkOS.Linux
+  else
+    return SdkOS.Windows
+}
+
+it('should download the SDK', async () => {
+  const cacheDir = path.join(process.cwd(), 'target/.cache')
+  const targetDir = path.join(process.cwd(), 'target/download')
+  const downloader = await createDownloader({
+    url: {
+      arch: getArch(),
+      os: getOS(),
+      version: SdkVersion.API12,
+    },
+    cacheDir,
+    targetDir,
+  })
+
+  let totalPercentage = 0
+  downloader.on('download-progress', (progress) => {
+    console.warn(`Downloading ${progress.percentage}... ${progress.network}${progress.unit}/s`)
+    totalPercentage += progress.increment
+  })
+  downloader.on('tar-extracted', (entry) => {
+    console.warn(`Extracting: ${entry.path}`)
+  })
+  downloader.on('zip-extracted', (entry) => {
+    console.warn(`Extracting: ${entry.path}`)
+  })
+
+  await downloader.startDownload()
+  expect(totalPercentage).toBe(100)
+  expect(fs.existsSync(cacheDir)).toBe(true)
+  await downloader.checkSha256()
+  await downloader.extractTar()
+  await downloader.extractZip()
+  expect(fs.existsSync(targetDir)).toBe(true)
+  await downloader.clean()
+  expect(fs.readdirSync(cacheDir)).toHaveLength(0)
+
+  // Timeout: 20 min
+}, 20 * 60 * 1000)
