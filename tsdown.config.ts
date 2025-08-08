9c4e34a73aca9a888c0c775de8964bc5faf3ce41
groupguanfang <1203970284@qq.com>
2025-08-08 17:15:31 +0800

    chore: fix tsdown config

--- tsdown.config.ts
+++ tsdown.config.ts
@@ -1,8 +1,7 @@
-import process from 'node:process'
 import { defineConfig } from 'tsdown'
 
 export default defineConfig({
-  entry: process.env.CI ? ['./src/github-action.ts'] : ['./src/index.ts', './src/cli.ts'],
+  entry: ['./src/github-action.ts', './src/index.ts', './src/cli.ts'],
   sourcemap: true,
   dts: true,
   format: ['esm', 'cjs'],
